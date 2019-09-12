2f2cd4386d4f99badb9c332bb75d52adeed41933
cloudwy <wuyun_0413@yahoo.com>
2019-09-12 16:36:35 +0200

    visualization for AE model

--- visualization_acc.py
+++ visualization_acc.py
@@ -4,14 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # calculate mean and variance
-mean_GR = [0.994785,0.813635,0.649825,0.507655,0.42498]
-print(mean_GR)
-var_GR = [5.1595E-05,0.020253227,0.006657944,0.009077331,0.001999509]
-print(var_GR)
-mean_noGR = [0.994585,0.640325,0.48431,0.387605,0.35388]
+mean_noGR = [0.99794,0.95434,0.81535,0.774405,0.615463]
 print(mean_noGR)
-var_noGR = [3.41E-05,0.016615,0.011115,0.007188,0.003537]
+var_noGR = [1.64632E-07,9.06779E-06,0.00027201,0.000202887,0.000130327]
 print(var_noGR)
+
+mean_GR = [0.997715,0.973285,0.917495,0.891235,0.759785]
+print(mean_GR)
+var_GR = [3.61342E-07,4.42239E-06,8.15816E-05,8.72834E-05,0.000202923]
+print(var_GR)
+
+mean_upp = [0.99739,0.971695,0.92973,0.91317,0.80011]
+print(mean_upp)
+var_upp = [4.17789E-07,7.65839E-06,5.42275E-05,7.18348E-05,0.000147218]
+print(var_upp)
 
 # plot
 xdata = np.array(['1','2','3','4','5'])
@@ -19,14 +25,15 @@ ydata_mean_GR = mean_noGR
 
 plt.errorbar(xdata,mean_noGR,yerr=var_noGR,fmt='x',color='green', ecolor='lightgreen',elinewidth=3,capsize=0)
 plt.errorbar(xdata,mean_GR,yerr=var_GR,fmt='o',color='red', ecolor='pink',elinewidth=3,capsize=0)
+plt.errorbar(xdata,mean_upp,yerr=var_upp,fmt='+',color='blue', ecolor='lightblue',elinewidth=3,capsize=0)
 #mean_all = 0.7848
 #plt.hlines(mean_all,xdata[0],xdata[4],colors='lightgray')
 
-plt.title('AAE1_MNIST')
+plt.title('AE_MNIST')
 plt.xlabel('Task')
 plt.ylabel('Acc')
-#plt.legend(['all_Data','without_GR', 'with_GR'])
-plt.legend(['no_GR', 'with_GR'])
+#plt.legend(['without_GR', 'with_GR'])
+plt.legend(['lower bound', 'with_GR','upper bound'])
 plt.show()
 
 
